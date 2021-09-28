4940673de69e96de648c8ef12b8d5a51e8320023
Mikel Jagan <mikelmnj@gmail.com>
2021-09-28 12:26:42 -0400

    Added Banner component.

--- src/scenes/Feedback/SectionBanner.js
+++ src/scenes/Feedback/SectionBanner.js
@@ -2,8 +2,11 @@ import React, { Fragment } from "react";
 import { bannerCode } from './codeSamples';
 import { bannerTable } from './tables';
 import { buildRows } from 'helpers/tableHelpers';
+import { codeSnippet } from 'helpers/utilityHelpers';
 import Table from 'components/Table/Table';
 import SampleBox from 'components/SampleBox/SampleBox';
+import Banner from 'components/Banner/Banner';
+
 const SectionBanner = props => {
   return (
     <Fragment>
@@ -14,13 +17,21 @@ const SectionBanner = props => {
           {buildRows(bannerTable)}
         </Table>
 
-        <p>
-          Additional text...
-        </p>
+        <strong>Note</strong>: The {codeSnippet("<Banner />")} component will take up
+        all of the available width in its parent container by default.
       </div>
 
       <SampleBox name="Banner" code={bannerCode}>
+        <span>
+          <Banner text="The floggings will continue until morale improves..." />
+        </span>
 
+        <span>
+          <Banner noIcon center className="customBanner">
+            <i className="fas fa-exclamation" />&nbsp;&nbsp;
+            <strong>Reminder</strong>: Servers will be down for a bit.
+          </Banner>
+        </span>
       </SampleBox>
     </Fragment>
   );
