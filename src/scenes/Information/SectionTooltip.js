b636f7e37149838787ee8056a108bf012f4c5720
Mikel Jagan <mikelmnj@gmail.com>
2021-10-08 12:57:07 -0400

    Added Tooltip component and documentation.

--- src/scenes/Information/SectionTooltip.js
+++ src/scenes/Information/SectionTooltip.js
@@ -6,6 +6,18 @@ import { codeSnippet } from 'helpers/utilityHelpers';
 import Table from 'components/Table/Table';
 import SampleBox from 'components/SampleBox/SampleBox';
 import Tooltip from 'components/Tooltip/Tooltip';
+import colors from 'theme/colors.scss';
+
+const css = `.customTooltip .tip {
+  font-weight: bold;
+  text-align: center;
+  color: white;
+  background-color: #0d97ff;
+}
+
+.customTooltip i {
+  color: #0d97ff;
+}`;
 
 const SectionTooltip = props => {
   return (
@@ -16,10 +28,39 @@ const SectionTooltip = props => {
         <Table headers={[ "NAME", "DESCRIPTION", "DEFAULT" ]}>
           {buildRows(tooltipTable)}
         </Table>
+
+        <strong>Note</strong>: When passing a custom <i>className</i> to the
+        {codeSnippet("<Tooltip />")} component, you will need to target the
+        {codeSnippet(".tip")} and {codeSnippet("i")} selectors with CSS chaining:
+
+        {codeSnippet(css, true)}
       </div>
 
       <SampleBox name="Tooltip" code={tooltipCode}>
+        <div className="column">
+          <div className="inline">
+            Standard&nbsp;
+            <Tooltip text="Standard tool tip." />
+          </div>
 
+          <div className="inline">
+            Child&nbsp;
+            <Tooltip position="top">
+              <i className="fas fa-smile" />&nbsp;
+              Tool tip rendering children.
+            </Tooltip>
+          </div>
+
+          <div className="inline">
+            Custom&nbsp;
+            <Tooltip
+              icon="fas fa-question-circle"
+              text="Customized tool tip."
+              position="top"
+              className="customTooltip"
+            />
+          </div>
+        </div>
       </SampleBox>
     </Fragment>
   );
