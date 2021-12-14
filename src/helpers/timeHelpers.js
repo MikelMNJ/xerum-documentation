2d50f5931f5964283958cd172e19aa9bb2997f93
Mikel Jagan <mikelmnj@gmail.com>
2021-12-14 11:17:50 -0500

    Added sub and micro time.

--- src/helpers/timeHelpers.js
+++ src/helpers/timeHelpers.js
@@ -6,18 +6,18 @@ import { startCase } from 'lodash';
 const format = "ddd, l, hh:mm:ss A";
 
 export const counter = args => {
-  const { timeframe: frame, aggregate, local, end: endDate, details } = args
+  const { timeframe: frame, aggregate, local, end: endDate, details, callback } = args
   const timeframe = frame || "hours";
   const agg = aggregate || 1;
   const now = local ? moment() : moment().utc();
   const prev = moment(now).startOf(parentTime());
   const next = moment(prev).add(1, parentTime());
-  const units = next.diff(prev, timeframe);
-  const totalTimes = units / agg;
+  const units = next?.diff(prev, timeframe);
+  const totalTimes = (units / agg) | 0;
   const times = [];
   let mostRecent;
   let aggCount;
-  let count = { remaining: "00:00:00" };
+  let count = {};
 
   // Establish broader range to break-down time brackets.
   function parentTime() {
@@ -38,6 +38,100 @@ export const counter = args => {
       default:
         return "day";
     }
+  };
+
+  function childTime(micro) {
+    // Default requests units for the middle time: 00:xx:00
+    // Micro requests units for the smallest time 00:00:xx
+
+    switch (timeframe) {
+      case "years":
+        return micro ? "weeks" : "months";
+      case "months":
+        return micro ? "days" : "weeks";
+      case "weeks":
+        return micro ? "hours" : "days";
+      case "days":
+        return micro ? "minutes" : "hours";
+      case "hours":
+        return micro ? "seconds" : "minutes";
+      case "minutes":
+        return micro ? "milliseconds" : "seconds";
+      case "seconds":
+        return "milliseconds";
+
+      default:
+        return micro ? "seconds" : "minutes";
+    }
+  };
+
+  function abbreviatedTime(time) {
+    switch(time) {
+      case "milliseconds":
+        return "ms";
+      case "seconds":
+        return "s";
+      case "minutes":
+        return "m";
+      case "hours":
+        return "h";
+      case "days":
+        return "D";
+      case "weeks":
+        return "W";
+      case "months":
+        return "M";
+      case "years":
+        return "Y";
+
+      default:
+        return "h";
+    }
+  }
+
+  function fixedUnits(micro) {
+    // Default requests units for the middle time: 00:xx:00
+    // Micro requests units for the smallest time 00:00:xx
+
+    switch(timeframe) {
+      case "seconds":
+        return 1000;
+      case "minutes":
+        return micro ? 1000 : 60;
+      case "hours":
+        return 60; // micro and standard the same: 60s/m vs 60m/hr
+      case "days":
+        return micro ? 60 : 24;
+      case "weeks":
+        return micro ? 24 : 7;
+      case "months":
+        return micro ? 7 : 4.34524; // TODO: Check the accuracy of this value.
+      case "years":
+        return micro ? 4.34524 : 12;
+
+      default:
+        return 60;
+    }
+  }
+
+  const remainingTime = () => {
+    const floatTime = end?.diff(now, timeframe, true);
+    const floatIsolated = floatTime - Math.floor(floatTime);
+    const subTime = fixedUnits() * floatIsolated;
+    const subIsolated = subTime - Math.floor(subTime);
+    const microTime = fixedUnits(true) * subIsolated;
+    const first = end?.diff(now, timeframe) + abbreviatedTime(timeframe);
+    const second = timeframe !== ("seconds") && (subTime | 0) + abbreviatedTime(childTime());
+    const third = timeframe !== ("minutes") && (microTime | 0) + abbreviatedTime(childTime(true));
+
+    const time = () => {
+      if (!second) return `${first}`;
+      if (!third) return `${first} ${second}`;
+
+      return `${first} ${second} ${third}`;
+    };
+
+    return time();
   };
 
   // Establish start/end times for time brackets using timeframe and aggregation.
@@ -66,7 +160,7 @@ export const counter = args => {
   if (endDate) {
     count.remaining = "Fixed end time...";
   } else if (timeframe) {
-    count.remaining = `${end.diff(now, timeframe)} ${timeframe} remaining`;
+    count.remaining = remainingTime();
 
     if (details) {
       count.details =
