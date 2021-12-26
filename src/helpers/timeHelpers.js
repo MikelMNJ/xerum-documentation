3b575439120a9b788067edfd5e5fcf8f758ee464
Mikel Jagan <mikelmnj@gmail.com>
2021-12-26 11:42:49 -0500

    Moved functions around.

--- src/helpers/timeHelpers.js
+++ src/helpers/timeHelpers.js
@@ -53,6 +53,29 @@ export const aggTime = args => {
     }
   };
 
+  // Establish start/end times for time brackets using timeframe and aggregation.
+  for (let i = 0; i < totalTimes; i++) {
+    const workingTime = moment(prev).add(i * agg, timeframe);
+    times.push(workingTime);
+  }
+
+  // Find time bracket we are currently in.
+  times.find((val, index) => {
+    if (now >= val && (index === times.length - 1)) {
+      // Last time period in array.
+      mostRecent = times[times.length - 1];
+      aggCount = index + 1;
+      return times[times.length - 1];
+    } else if (now < val && (index !== 0 || index !== times.length - 1)) {
+      // First and every time period in between.
+      mostRecent = times[index - 1];
+      aggCount = index;
+      return times[index - 1];
+    }
+  });
+
+  const end = mostRecent && moment(mostRecent).add(agg, timeframeValid(timeframe) || "hours");
+
   const remainingTime = () => {
     const firstTime = end?.diff(now, timeframe);
     const firstTimeFloat = end?.diff(now, timeframe, true);
@@ -142,7 +165,7 @@ export const aggTime = args => {
       return isolated;
     };
 
-    function time() {
+    const time = () => {
       // TODO: Make formatting a single function, including space additions.
       const space = compact ? ":" : " ";
 
@@ -162,29 +185,6 @@ export const aggTime = args => {
 
     return time();
   };
-
-  // Establish start/end times for time brackets using timeframe and aggregation.
-  for (let i = 0; i < totalTimes; i++) {
-    const workingTime = moment(prev).add(i * agg, timeframe);
-    times.push(workingTime);
-  }
-
-  // Find time bracket we are currently in.
-  times.find((val, index) => {
-    if (now >= val && (index === times.length - 1)) {
-      // Last time period in array.
-      mostRecent = times[times.length - 1];
-      aggCount = index + 1;
-      return times[times.length - 1];
-    } else if (now < val && (index !== 0 || index !== times.length - 1)) {
-      // First and every time period in between.
-      mostRecent = times[index - 1];
-      aggCount = index;
-      return times[index - 1];
-    }
-  });
-
-  const end = mostRecent && moment(mostRecent).add(agg, timeframeValid(timeframe) || "hours");
 
   if (timeframe) {
     const newRange = now.diff(mostRecent, "seconds") === 0;
