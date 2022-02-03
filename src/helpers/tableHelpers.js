0c8e9fb74ecc0af8c698452a529c6462ec6fe928
Mikel Jagan <mikelmnj@gmail.com>
2022-02-03 12:35:42 -0500

    Headers now precede table data after header is hidden on mobile.

--- src/helpers/tableHelpers.js
+++ src/helpers/tableHelpers.js
@@ -4,14 +4,17 @@ import TRow from 'components/Table/TRow';
 import TData from 'components/Table/TData';
 import colors from 'theme/colors.scss';
 
-export const headers = [ "NAME", "DESCRIPTION", "DEFAULT" ];
+export const headers = [ "PROP NAME", "DESCRIPTION", "DEFAULT" ];
 
 const buildCells = (obj, headers) => {
   return Object.values(obj).map((val, index) => (
     <TData key={index}>
-      <strong>
-        {startCase(headers[index].toLowerCase())}
-      </strong>: &nbsp;
+      <p className="respHeader">
+        <strong>
+          {startCase(headers[index].toLowerCase())}
+        </strong>: &nbsp;
+      </p>
+
       {val}
     </TData>
   ));
