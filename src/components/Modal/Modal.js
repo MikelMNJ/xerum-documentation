e2c93b180421936f18469ac19e1a374c7dc1058c
Mikel Jagan <mikelmnj@gmail.com>
2022-02-11 12:07:01 -0500

    Added Modal component.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { Fragment, useRef } from 'react';
+import { iconValid, hexValid } from 'helpers/validators';
+import { dismiss, slideIn, fadeIn } from 'helpers/animations';
+import Button from 'components/Button/Button';
 import './Modal.scss';
 
 const Modal = props => {
-  const { rest } = props;
+  const {
+    title,
+    titleColor,
+    bgClose,
+    closeIcon,
+    onClose,
+    className,
+    children,
+    rest
+  } = props;
+
+  const titleStyle = { color: hexValid(titleColor) };
+  const ref = useRef();
+  const bgRef = useRef();
+  const args = { onClose, targets: [ ref, bgRef ], time: 200 };
+
+  const buildClasses = () => {
+    let classList = `modal ${slideIn}`;
+    if (className) classList += ` ${className}`;
+    return classList;
+  };
 
   return (
-    <div {...rest}>
-      Modal
-    </div>
+    <Fragment>
+      <div
+        ref={bgRef}
+        className={`modalBG ${fadeIn}`}
+        onClick={() => {
+          if (bgClose) dismiss(args);
+        }}
+      />
+
+      <div ref={ref} className={buildClasses()} {...rest}>
+        <div className="header">
+          <h3 style={titleStyle}>{title}</h3>
+
+          <Button
+            className="closeBtn"
+            icon={closeIcon || iconValid("fa-solid fa-xmark")}
+            btnType="transparent"
+            hoverStyle={titleStyle}
+            callback={() => dismiss(args)}
+          />
+        </div>
+
+        {children}
+      </div>
+    </Fragment>
   );
 };
 
--- src/scenes/Containers/SectionModal.js
+++ src/scenes/Containers/SectionModal.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { modalCode } from './codeSamples';
 import { modalTable } from './tables';
 import { buildRows } from 'helpers/tableHelpers';
 import { codeSnippet } from 'helpers/utilityHelpers';
+import { dismiss } from 'helpers/animations';
+import Button from 'components/Button/Button';
 import SampleBox from 'components/SampleBox/SampleBox';
 import Modal from 'components/Modal/Modal';
 import Table from 'components/Table/Table';
+import colors from 'theme/colors.scss';
 
 const SectionModal = props => {
+  const [ visible, setVisible ] = useState(false);
+
+  const toggle = () => {
+    const modal = document.querySelector(".modal");
+    const modalBG = document.querySelector(".modalBG");
+    const args = {
+      onClose: () => setVisible(!visible),
+      targets: [ modal, modalBG ],
+    };
+
+    if (visible) return dismiss(args);
+    setVisible(!visible);
+  };
+
   return (
     <Fragment>
       <div>
@@ -17,7 +34,20 @@ const SectionModal = props => {
       </div>
 
       <SampleBox name="Modal" code={modalCode}>
-        <Modal />
+        <Button
+          text={`${visible ? "Hide" : "Show"}`}
+          callback={toggle}
+        />
+
+        {visible && (
+          <Modal
+            title="Modal title..."
+            onClose={() => setVisible(!visible)}
+            bgClose
+          >
+            <p>Modal content...</p>
+          </Modal>
+        )}
       </SampleBox>
     </Fragment>
   );
