b7d7efd4c9a5ab4e0aa9017790f7d2fca43b2f48
Mikel Jagan <mikelmnj@gmail.com>
2022-02-14 22:11:04 -0500

    Implemented first pass at Notification transitions.
    There is an outstanding bug where transitions break if closing notifications from oldest to newest  (top to bottom).  Manually closing from newest to oldest (bottom to top) works as expected.

--- src/components/Notification/Messages.js
+++ src/components/Notification/Messages.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { createRef, useEffect, useRef } from 'react';
+import { dismiss } from 'helpers/animations';
 import Notification from './Notification';
 import './Notification.scss';
 
+
 const Messages = props => {
   const { icon, noIcon, noTime, time, messages, setMessages, ...rest } = props;
+  const targets = useRef([]);
 
   useEffect(() => {
     if (!noTime && messages?.length >= 1) {
       const timer = setTimeout(() => {
-        handleClose();
+        dismiss({ targets: [ targets.current[0] ], onClose: removeMessage });
         clearTimeout(timer);
       }, time || 4000);
 
@@ -16,20 +19,24 @@ const Messages = props => {
     }
   }, [messages]);
 
-  const handleClose = i => {
+  function removeMessage(i) {
     const updatedMsgs = [ ...messages ];
-    updatedMsgs.splice(i, 1);
+    updatedMsgs.splice(i || 0, 1);
     if (setMessages) setMessages(updatedMsgs);
   };
 
   const buildMessages = () => {
+    const refs = messages?.map((msg, i) => targets.current[i] ?? createRef());
+    targets.current = [ ...refs ];
+
     return messages?.map((msg, i) => {
       return (
         <Notification
           key={i}
+          targets={targets.current}
+          ref={targets.current[i]}
           message={msg}
-          index={i}
-          close={handleClose}
+          args={{ targets: [ targets.current[i] ], onClose: () => removeMessage(i) }}
           {...rest}
         />
       );
