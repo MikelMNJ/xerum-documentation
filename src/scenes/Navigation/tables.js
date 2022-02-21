1a8bc2184e093af86a40f4646a5f85ea3b4b6341
Mikel Jagan <mikelmnj@gmail.com>
2022-02-21 13:26:14 -0500

    Added props to table and code samples.

---
+++ src/scenes/Navigation/tables.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react';
+import { headers } from 'helpers/tableHelpers';
+
+export const menuTable = {
+  headers,
+  rows: [
+    {
+      tData1: "",
+      tData2: "",
+      tData3: "-"
+    },
+  ],
+};
+
+export const paginationTable = {
+  headers,
+  rows: [
+    {
+      tData1: "totalPages",
+      tData2: "Total page count, as received from your server.",
+      tData3: 1
+    },
+    {
+      tData1: "currentPage",
+      tData2: "Current page, as received from your server.",
+      tData3: 1
+    },
+    {
+      tData1: "onPageChange",
+      tData2:
+        <Fragment>
+          Generic event handler when page number/icons or prev/next buttons
+          are clicked. Receives new page number for your API page data request.
+        </Fragment>,
+      tData3: "-"
+    },
+    {
+      tData1: "prevIcon",
+      tData2:
+        <Fragment>
+          Fontawesome i.e. "<strong>fa-solid fa-angles-left</strong>".
+        </Fragment>,
+      tData3: <i className="fa-solid fa-angles-left" />
+    },
+    {
+      tData1: "pageIcon",
+      tData2:
+        <Fragment>
+          Fontawesome i.e. "<strong>fa-solid fa-circle</strong>".
+        </Fragment>,
+      tData3: <i className="fa-solid fa-circle" />
+    },
+    {
+      tData1: "nextIcon",
+      tData2:
+        <Fragment>
+          Fontawesome i.e. "<strong>fa-solid fa-angles-right</strong>".
+        </Fragment>,
+      tData3: <i className="fa-solid fa-angles-right" />
+    },
+  ],
+};
